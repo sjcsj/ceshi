bcc77e7e98ad02aec8ab8d9fbb353c4874c27981
sjcsj <96323397+sjcsj@users.noreply.github.com>
2022-09-30 22:09:56 +0800

    Update Wuhu.java

--- ceshi1/src/com/wuhu/Wuhu.java
+++ ceshi1/src/com/wuhu/Wuhu.java
@@ -4,5 +4,6 @@ public class Wuhu {
     public static void main(String[] args) {
         System.out.println("ahahahahahaa");
         System.out.println("niubi");
+        System.out.println("666");
     }
 }
